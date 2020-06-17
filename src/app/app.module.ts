8252ee2e322793a3be7ebd4a58f387a14273162d
Lucas Rosello <l.martin.rosello@gmail.com>
2020-06-17 00:24:32 -0300

    añado los dos headers a angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderDesktopComponent } from './header-desktop/header-desktop.component';
+import { HeaderMobileComponent } from './header-mobile/header-mobile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeaderDesktopComponent,
+    HeaderMobileComponent
   ],
   imports: [
     BrowserModule,
