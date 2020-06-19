0e11f0ef0b77331260edc64bdb3f0e4051bccfd7
Lucas Rosello <l.martin.rosello@gmail.com>
2020-06-18 22:39:45 -0300

    limpio y añado los links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 const routes: Routes = [
-  // {path:"", component:InicioComponent}
+  {path:"", component: InicioComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderDesktopComponent } from './header-desktop/header-desktop.component';
 import { HeaderMobileComponent } from './header-mobile/header-mobile.component';
@@ -12,7 +11,6 @@ import { InicioComponent } from './inicio/inicio.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HeaderDesktopComponent,
     HeaderMobileComponent,
     InicioComponent
