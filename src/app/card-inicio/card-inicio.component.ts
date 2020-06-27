b0094e64a57c8836274006f39a45b99972a02171
Lucas Rosello <l.martin.rosello@gmail.com>
2020-06-27 00:09:05 -0300

    pruebo params e input - incompleto

--- src/app/card-inicio/card-inicio.component.ts
+++ src/app/card-inicio/card-inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NoticiasService } from '../servicios/noticias/noticias.service';
 
 @Component({
@@ -7,29 +7,11 @@ import { NoticiasService } from '../servicios/noticias/noticias.service';
   styleUrls: ['./card-inicio.component.css']
 })
 export class CardInicioComponent implements OnInit {
-
-  contacts: Observable<Contact[]>;
-  arrayNoticias = Observable.of([
-    {
-      "destacado": false,
-      "_id": "5ef421c5d4e37b3f448a5ee8",
-      "verificado": false,
-      "titulo": "ay caramba",
-      "descripcion": "esto es traido a ustedes por buzz cola",
-      "link": "https://www.google.com",
-      "__v": 0
-    },
-    {
-      "destacado": false,
-            "_id": "5ef421fad4e37b3f448a5ee9",
-            "verificado": false,
-            "titulo": "ay caramba",
-            "descripcion": "esto es traido a ustedes por buzz cola",
-            "link": "https://www.google.com",
-            "__v": 0
-    }
-  ])
-  titulo = "Novedades de PHP 7.3"
+  
+  @Input()
+  itemNoticia
+  
+  titulo = this.itemNoticia["titulo"]
   descripcion = "Hoy mientras comía me he visto esta charla de Rasmus Lerdorf (el creador de PHP)…"
   link = "https://medium.com/@ger86/novedades-de-php-7-3-9f603f4885e0";
   destacado = false;
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  arrayNoticias = [
+    {
+      destacado: false,
+      _id: "5ef421c5d4e37b3f448a5ee8",
+      verificado: false,
+      titulo: "ay caramba",
+      descripcion: "esto es traido a ustedes por buzz cola",
+      link: "https://www.google.com",
+      __v: 0
+    },
+    {
+      destacado: false,
+      _id: "5ef421c5d4e37b3f448a5ee8",
+      verificado: false,
+      titulo: "ay caramba",
+      descripcion: "esto es traido a ustedes por buzz cola",
+      link: "https://www.google.com",
+      __v: 0
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
