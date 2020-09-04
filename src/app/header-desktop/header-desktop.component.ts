bfe0d4e3ce86a85a26488d8e4eafd72e39168620
lucas <l.martin.rosello@gmail.com>
2020-09-04 09:24:56 -0300

    agrego routing, cierre de sesion y dropdown en el boton de usuario

--- src/app/header-desktop/header-desktop.component.ts
+++ src/app/header-desktop/header-desktop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; //ROUTING
 
 @Component({
   selector: 'app-header-desktop',
@@ -10,10 +11,17 @@ export class HeaderDesktopComponent implements OnInit {
   usuario = localStorage.getItem("usuario")
   tokenExists = true //if(localStorage.getItem("token")){ return true };
 
-  constructor() { }
+  constructor(private router: Router) { } //ROUTING
 
   ngOnInit(): void {
     
+  }
+
+  cerrarSesion(){
+    this.usuario = null
+    this.tokenExists = false
+    localStorage.clear()
+    this.router.navigate(['/']); //ROUTING
   }
 
 }
