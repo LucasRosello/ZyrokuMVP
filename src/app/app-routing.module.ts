62a7ee3d363778892cc4112cd3d802b40bd1b18c
lucas <l.martin.rosello@gmail.com>
2020-09-27 22:20:22 -0300

    agrego landing a angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { InicioComponent } from './inicio/inicio.component';
 import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 import { ReproductorComponent } from './reproductor/reproductor.component';
 import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
 
 
 const routes: Routes = [
   {path:"", component: InicioComponent},
+  {path:"landing", component: LandingComponent},
   {path:"stats", component: EstadisticasComponent},
   {path:"view/:id", component: ReproductorComponent},
   {path:"loguear", component: LoginComponent}
